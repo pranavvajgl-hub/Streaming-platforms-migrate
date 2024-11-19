845bd703b715a0a2247c9f766cd87e18090d8c06
vajglos <vajgl.pranav123@gmail.com>
2024-11-19 14:50:25 +0100

    working code for authentification and getting playlist "stuff"

--- main.py
+++ main.py
@@ -23,7 +23,37 @@ spotify_playlists = spotify_client.get_playlists()
 youtube = ytmusicauth.get_authenticated_service()
 youtube_playlists = youtube.playlists().list(part="snippet", mine=True).execute()
 
+# PLAYLIST COMPARISON
 for spotify_playlist in spotify_playlists:
     for youtube_playlist in youtube_playlists['items']:
         if spotify_playlist['name'] == youtube_playlist['snippet']['title']:
-            spotify_tracks = spotify_client.get_playlist_tracks()
+            spotify_tracks = spotify_client.get_playlist_tracks(spotify_playlist['id'])
+
+            youtube_tracks = youtube.playlistItems().list(
+                part="snippet", playlistId=youtube_playlist['id'], maxResults=100
+            ).execute()
+
+            for spotify_track in spotify_tracks['items']:
+                spotify_track.get_youtube_id(youtube)
+                found = False
+                for youtube_track in youtube_tracks['items']:
+                    #and spotify_track.artist == youtube_track["snippet"]["videoOwnerChannelTitle"]
+                    #pripis to nahore, jestli to nepujde...
+                    if spotify_track.youtube_id == youtube_track['snippet']['resourceId']['title']:
+                        found = True
+                        break
+                    if not found:
+                        request = youtube.playlistItems().insert(
+                            part="snippet",
+                            body={
+                                "snippet": {
+                                    "playlistId": youtube_playlist['id'],
+                                    "resourceId": {
+                                        "kind": "youtube#video",
+                                        "videoId": spotify_track.get_video_id(youtube)
+                                    }
+                                }
+                            }
+                        )
+                        response = request.execute()
+                        print(f"Pridana skladba {spotify_track.title} do playlistu {youtube_playlist['snippet']['title']}")
