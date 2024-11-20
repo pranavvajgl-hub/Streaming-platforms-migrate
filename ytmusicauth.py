f379d8d34a23f0b40e1f3cfa4f5301d5e9b41345
vajglos <vajgl.pranav123@gmail.com>
2024-11-20 22:07:45 +0100

    Creating YouTubeMusic class

--- ytmusicauth.py
+++ ytmusicauth.py
@@ -8,9 +8,19 @@ CLIENT_SECRETS_FILE = 'credentials.json'
 SCOPES = ['https://www.googleapis.com/auth/youtube']
 API_KEY = os.getenv('API_KEY')
 
-def get_authenticated_service(client_secrets_file, scopes, youtube_api_service_name, youtube_api_version):
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-    credentials = flow.run_local_server(port=0)
-    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, credentials=credentials)
-    return youtube
+class YouTubeMusic:
+    def __init__(self, client_secrets_file, scopes, youtube_api_service_name, youtube_api_version):
+        self.client_secrets_file = client_secrets_file
+        self.scopes = scopes
+        self.youtube_api_service_name = youtube_api_service_name
+        self.youtube_api_version = youtube_api_version
+        self.youtube = self.get_authenticated_service()
 
+    def get_authenticated_service(self):
+        """
+        Initialize YouTube API and returns object for API access
+        """
+        flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+        credentials = flow.run_local_server(port=0)
+        youtube = build(self.youtube_api_service_name, self.youtube_api_version, credentials=credentials)
+        return youtube
