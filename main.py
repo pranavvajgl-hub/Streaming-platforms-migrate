06160a1fcf6cbd940740e94b3363f8b2c11e3083
vajglos <vajgl.pranav123@gmail.com>
2024-11-25 15:40:59 +0100

    Creating main function in main.py

--- main.py
+++ main.py
@@ -53,140 +53,141 @@ progress = get_last_progress()
 # Loading Spotify playlists:
 playlists = spotify.get_playlists()
 
-# In every playlist
-for playlist in playlists['items']:
-    try:
-        # See if the album does exist in Spotify
-        playlist_title = playlist['name']
-        if playlist_title in progress:
-            playlist_id = progress[playlist_title]["id"]
-            last_track_index = progress[playlist_title]["last_track_index"]
-            print(f"Playlist '{playlist_title}' does exist. Continue with index {last_track_index}.")
-            logging.info(f"Playlist '{playlist_title}' does exist. Continue with index {last_track_index}.")
-        else:
-            # Creating a new album on YT Music
-            time.sleep(1)
-            playlist_response = youtube.playlists().insert(
-                part="snippet,status",
-                body={
-                    "snippet": {
-                        "title": playlist_title,
-                        "description": f"Playlist form Spotify",
+def main():
+    # In every playlist
+    for playlist in playlists['items']:
+        try:
+            # See if the album does exist in Spotify
+            playlist_title = playlist['name']
+            if playlist_title in progress:
+                playlist_id = progress[playlist_title]["id"]
+                last_track_index = progress[playlist_title]["last_track_index"]
+                print(f"Playlist '{playlist_title}' does exist. Continue with index {last_track_index}.")
+                logging.info(f"Playlist '{playlist_title}' does exist. Continue with index {last_track_index}.")
+            else:
+                # Creating a new album on YT Music
+                time.sleep(1)
+                playlist_response = youtube.playlists().insert(
+                    part="snippet,status",
+                    body={
+                        "snippet": {
+                            "title": playlist_title,
+                            "description": f"Playlist form Spotify",
+                        },
+                        "status": {
+                            "privacyStatus": "private"
+                        }
                     },
-                    "status": {
-                        "privacyStatus": "private"
-                    }
-                },
-                key=API_KEY
-            ).execute()
-            playlist_id = playlist_response['id']
-            print(f"Playlist created on YouTube: {playlist_title} ({playlist_id})")
-            logging.info(f"Playlist created on YouTube: {playlist_title} ({playlist_id})")
+                    key=API_KEY
+                ).execute()
+                playlist_id = playlist_response['id']
+                print(f"Playlist created on YouTube: {playlist_title} ({playlist_id})")
+                logging.info(f"Playlist created on YouTube: {playlist_title} ({playlist_id})")
 
-            # Saving progress
-            last_track_index = 0
-            progress[playlist_title] = {"id": playlist_id, "last_track_index": last_track_index}
-            save_progress(progress)
-            logging.info(f"Progress saved on index: {progress[playlist_title]}")
+                # Saving progress
+                last_track_index = 0
+                progress[playlist_title] = {"id": playlist_id, "last_track_index": last_track_index}
+                save_progress(progress)
+                logging.info(f"Progress saved on index: {progress[playlist_title]}")
 
-        # Initialization of tracks in playlist
-        tracks = sp.playlist_tracks(playlist['id'])
-        search_cache = {}
-        for i, track in enumerate(tracks['items']):
-            search_response = None
-            search_query = None
-            track_name = None
+            # Initialization of tracks in playlist
+            tracks = sp.playlist_tracks(playlist['id'])
+            search_cache = {}
+            for i, track in enumerate(tracks['items']):
+                search_response = None
+                search_query = None
+                track_name = None
 
-            # Skip last track
-            if i < last_track_index:
-                continue
-            try:
+                # Skip last track
+                if i < last_track_index:
+                    continue
                 try:
-                    # Track info on spotify
-                    track_name = track['track']['name']
-                    artist_name = track['track']['artists'][0]['name']
-                    search_query = f"{track_name} - {artist_name}"
-                    # Search on YTMusic
-                    if search_query in search_cache:
-                        search_response = search_cache[search_query]
-                        print(f"Result from search is form cache: {search_query}")
-                    else:
-                        search_response = youtube.search().list(
-                            q=search_query,
-                            part='id,snippet',
-                            type='video',
-                            maxResults=1,
-                            key=API_KEY
-                        ).execute()
-                        search_cache[search_query] = search_response
-                except HTTPError as e:
-                    if "quotaExceeded" in str(e):
-                        print("You have exceeded your quota for searching.")
-                        logging.info(f"You have exceeded your quota for searching.")
-                    else:
-                        print(f"Error with YouTube API: {e}")
-                        logging.info(f"Error with YouTube API: {e}")
-                        raise
-
-                # Adding playlist into YTMusic found on Spotify
-                if search_response['items']:
-                    video_id = search_response['items'][0]['id']['videoId']
-                    print(f"Track found on YouTube: {search_query} ({video_id})")
-
-                    max_retries = 3
-                    retries = 0
-                    while retries < max_retries:
-                        try:
-                            # Adding song into playlist
-                            playlist_item_response = youtube.playlistItems().insert(
-                                part="snippet",
-                                body={
-                                    "snippet": {
-                                        "playlistId": playlist_id,
-                                        "resourceId": {
-                                            "kind": "youtube#video",
-                                            "videoId": video_id
-                                        }
-                                    }
-                                },
+                    try:
+                        # Track info on spotify
+                        track_name = track['track']['name']
+                        artist_name = track['track']['artists'][0]['name']
+                        search_query = f"{track_name} - {artist_name}"
+                        # Search on YTMusic
+                        if search_query in search_cache:
+                            search_response = search_cache[search_query]
+                            print(f"Result from search is form cache: {search_query}")
+                        else:
+                            search_response = youtube.search().list(
+                                q=search_query,
+                                part='id,snippet',
+                                type='video',
+                                maxResults=1,
                                 key=API_KEY
                             ).execute()
-                            print(f"Track: {track_name} added into playlist {playlist_id}")
-                            logging.info(f"Track: {track_name} added into playlist {playlist_id}")
-                            # Saving progress
-                            progress[playlist_title]["last_track_index"] = i + 1
-                            save_progress(progress)
+                            search_cache[search_query] = search_response
+                    except HTTPError as e:
+                        if "quotaExceeded" in str(e):
+                            print("You have exceeded your quota for searching.")
+                            logging.info(f"You have exceeded your quota for searching.")
+                        else:
+                            print(f"Error with YouTube API: {e}")
+                            logging.info(f"Error with YouTube API: {e}")
+                            raise
 
-                        except HTTPError as e:
-                            if e.resp.status == 409 and "SERVICE_UNAVAILABLE" in str(e):
-                                retries += 1
-                                print(f"Error adding song (attempt {retries}/{max_retries}): {e}")
-                                logging.info(f"Error adding song (attempt {retries}/{max_retries}): {e}")
-                                time.sleep(2)
-                            else:
-                                print(f"Error with YouTube API: {e}")
-                                logging.info(f"Error with YouTube API: {e}")
-                                raise
+                    # Adding playlist into YTMusic found on Spotify
+                    if search_response['items']:
+                        video_id = search_response['items'][0]['id']['videoId']
+                        print(f"Track found on YouTube: {search_query} ({video_id})")
 
-                        break
+                        max_retries = 3
+                        retries = 0
+                        while retries < max_retries:
+                            try:
+                                # Adding song into playlist
+                                playlist_item_response = youtube.playlistItems().insert(
+                                    part="snippet",
+                                    body={
+                                        "snippet": {
+                                            "playlistId": playlist_id,
+                                            "resourceId": {
+                                                "kind": "youtube#video",
+                                                "videoId": video_id
+                                            }
+                                        }
+                                    },
+                                    key=API_KEY
+                                ).execute()
+                                print(f"Track: {track_name} added into playlist {playlist_id}")
+                                logging.info(f"Track: {track_name} added into playlist {playlist_id}")
+                                # Saving progress
+                                progress[playlist_title]["last_track_index"] = i + 1
+                                save_progress(progress)
 
-                    if retries == max_retries:
-                        print(f"Failed to add after {max_retries} retries")
-                        logging.info(f"Failed to add after {max_retries} retries")
-                else:
-                    print(f"Track found on YouTube: {search_query}")
-                    logging.info(f"Track found on YouTube: {search_query}")
+                            except HTTPError as e:
+                                if e.resp.status == 409 and "SERVICE_UNAVAILABLE" in str(e):
+                                    retries += 1
+                                    print(f"Error adding song (attempt {retries}/{max_retries}): {e}")
+                                    logging.info(f"Error adding song (attempt {retries}/{max_retries}): {e}")
+                                    time.sleep(2)
+                                else:
+                                    print(f"Error with YouTube API: {e}")
+                                    logging.info(f"Error with YouTube API: {e}")
+                                    raise
 
-            except HTTPError as e:
-                print(f"Error with YouTube API: {e}")
-                logging.info(f"Error with YouTube API: {e}")
-            except spotipy.SpotifyException as e:
-                print(f"Error with Spotify API: {e}")
-                logging.info(f"Error with Spotify API: {e}")
+                            break
 
-    except HTTPError as e:
-        print(f"Error with YouTube API: {e}")
-        logging.info(f"Error with YouTube API: {e}")
-    except spotipy.SpotifyException as e:
-        print(f"Error with Spotify API: {e}")
-        logging.info(f"Error with Spotify API: {e}")
+                        if retries == max_retries:
+                            print(f"Failed to add after {max_retries} retries")
+                            logging.info(f"Failed to add after {max_retries} retries")
+                    else:
+                        print(f"Track found on YouTube: {search_query}")
+                        logging.info(f"Track found on YouTube: {search_query}")
+
+                except HTTPError as e:
+                    print(f"Error with YouTube API: {e}")
+                    logging.info(f"Error with YouTube API: {e}")
+                except spotipy.SpotifyException as e:
+                    print(f"Error with Spotify API: {e}")
+                    logging.info(f"Error with Spotify API: {e}")
+
+        except HTTPError as e:
+            print(f"Error with YouTube API: {e}")
+            logging.info(f"Error with YouTube API: {e}")
+        except spotipy.SpotifyException as e:
+            print(f"Error with Spotify API: {e}")
+            logging.info(f"Error with Spotify API: {e}")
